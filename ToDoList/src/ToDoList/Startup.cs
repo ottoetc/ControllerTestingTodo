e52a02f443872844122745aa2bad44d4155977fb
Michael Anthony Smith <michaelanthonysmith00@gmail.com>
2016-04-18 09:06:14 -0700

    Adds data connection string

--- ToDoList/src/ToDoList/Startup.cs
+++ ToDoList/src/ToDoList/Startup.cs
@@ -6,15 +6,25 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.Entity;
 
 namespace ToDoList
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; set; }
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
         
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddEntityFramework().AddSqlServer().AddDbContext<ToDoListContext>(options => options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,11 +37,6 @@ namespace ToDoList
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
-            });
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
             });
         }
 
