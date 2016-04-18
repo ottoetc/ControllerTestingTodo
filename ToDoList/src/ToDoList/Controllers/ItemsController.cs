7928eea17b819c076c236a5a2eeb42d72411b33e
Michael Anthony Smith <michaelanthonysmith00@gmail.com>
2016-04-18 09:31:20 -0700

    add controller method to get details info also add view for details

--- ToDoList/src/ToDoList/Controllers/ItemsController.cs
+++ ToDoList/src/ToDoList/Controllers/ItemsController.cs
@@ -12,9 +12,16 @@ namespace ToDoList.Controllers
     public class ItemsController : Controller
     {
         private ToDoListContext db = new ToDoListContext();
+
         public IActionResult Index()
         {
             return View(db.Items.ToList());
+        }
+
+        public IActionResult Details(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(x => x.ItemId == id);
+            return View(thisItem);
         }
     }
 }
