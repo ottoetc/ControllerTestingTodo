a9d3c8018e276f5388789af92bb431e238de3deb
Michael Anthony Smith <michaelanthonysmith00@gmail.com>
2016-04-18 10:10:38 -0700

    add delete and edit actions and views

--- ToDoList/src/ToDoList/Controllers/ItemsController.cs
+++ ToDoList/src/ToDoList/Controllers/ItemsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using ToDoList.Models;
+using Microsoft.Data.Entity;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -33,6 +34,35 @@ namespace ToDoList.Controllers
         public IActionResult Create(Item item)
         {
             db.Items.Add(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(x => x.ItemId == id);
+            return View(thisItem);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Item item)
+        {
+            db.Entry(item).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(x => x.ItemId == id);
+            return View(thisItem);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(x => x.ItemId == id);
+            db.Items.Remove(thisItem);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
